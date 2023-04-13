292be6ca53e19cc0c4c6e814ec50d5ee52af39e4
Sergei Afonin <s.afonin@bankffin.ru>
2023-04-13 13:01:40 +0300

    add annotations

---
+++ src/main/java/ru/examples/reflection_and_annotations/annotation/example_marker/AnnotationExample.java
@@ -0,0 +1,37 @@
+package ru.examples.reflection_and_annotations.annotation.example_marker;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
+public class AnnotationExample {
+}
+
+// A marker annotation.
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyMarker { }
+
+class Marker {
+
+    // Annotate a method using a marker.
+    // Notice that no ( ) is needed.
+    @MyMarker
+    public static void myMeth() {
+        Marker ob = new Marker();
+
+        try {
+            Method m = ob.getClass().getMethod("myMeth");
+
+            // Determine if the annotation is present.
+            if(m.isAnnotationPresent(MyMarker.class))
+                System.out.println("MyMarker is present.");
+
+        } catch (NoSuchMethodException exc) {
+            System.out.println("Method Not Found.");
+        }
+    }
+
+    public static void main(String args[]) {
+        myMeth();
+    }
+}
