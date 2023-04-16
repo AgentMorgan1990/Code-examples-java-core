fd8ba3e5917321c1e3ce6f75337e2e82864970db
Sergei Afonin <s.afonin@bankffin.ru>
2023-04-16 10:37:44 +0300

    add io example

---
+++ src/main/java/ru/examples/core/depricated/applets/SimpleApplet.java
@@ -0,0 +1,16 @@
+package ru.examples.core.depricated.applets;
+
+import java.awt.*;
+import java.applet.*;
+
+        /*
+        <applet code= " SimpleApplet" width=200 height= бO>
+        </applet>
+        */
+
+public class SimpleApplet extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("Пpocтeйший аnлет", 20, 20);
+    }
+}
