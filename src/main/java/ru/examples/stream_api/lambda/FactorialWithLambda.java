754a84c4447ebb7e647c43aa9c7e881e29d1d370
Sergei Afonin <s.afonin@bankffin.ru>
2023-04-20 11:09:10 +0300

    add lambdas

---
+++ src/main/java/ru/examples/stream_api/lambda/FactorialWithLambda.java
@@ -0,0 +1,23 @@
+package ru.examples.stream_api.lambda;
+
+public class FactorialWithLambda {
+}
+
+interface NumericFunc {
+    int func(int n);
+}
+
+class BlockLambdaDemo {
+    public static void main(String[] args) {
+        NumericFunc numericFunc = n -> {
+            int result = 1;
+            for (int i = 1; i <= n; i++) {
+                result = i * result;
+            }
+            return result;
+        };
+
+        System.out.println("Факториал числа 3 равен " + numericFunc.func(3));
+        System.out.println("Факториал числа 5 равен " + numericFunc.func(5));
+    }
+}
---
+++ src/main/java/ru/examples/stream_api/lambda/ReverseStringExample.java
@@ -0,0 +1,23 @@
+package ru.examples.stream_api.lambda;
+
+public class ReverseStringExample {
+}
+
+interface StringFunc {
+    String func(String n);
+}
+
+class BlockLambda2 {
+
+    public static void main(String[] args) {
+        StringFunc reverse = str -> {
+            //для эффективности нужно добавить стрингбилдер
+            String s = "";
+            for (int i = str.length() - 1; i >= 0; i--) {
+                s += str.charAt(i);
+            }
+            return s;
+        };
+        System.out.println("Наоборот " + reverse.func("Наоборот"));
+    }
+}
